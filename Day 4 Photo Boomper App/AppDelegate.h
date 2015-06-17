7e553ac5bb8306bb082f67713d36bd811b1a7148
Nazifa Baktybayeva <nazifa.2212@gmail.com>
2015-06-17 10:52:46 +0600

    Initial Commit

---
+++ Day 4 Photo Boomper App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Day 4 Photo Boomper App
+//
+//  Created by Nazifa Baktybayeva on 17.06.15.
+//  Copyright (c) 2015 Nazifa Baktybayeva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
